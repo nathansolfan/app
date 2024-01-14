58f263d41e65c74ec6d233426953ddbdbcb7e620
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-14 23:44:49 +0000

    added the app.listen goes 2 params, port and func

--- server/server.js
+++ server/server.js
@@ -4,3 +4,8 @@ const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.post("submit-form-jo", (req, res) => {
+  console.log(req.body); // log the data
+  res.send("Data Received jo");
+});
