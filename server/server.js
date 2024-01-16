2d29266a61bea132152916009e5974e56df3e29c
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-16 00:08:18 +0000

    Local server is working correctly

--- server/server.js
+++ server/server.js
@@ -4,10 +4,13 @@ const cors = require("cors");
 const app = express();
 const port = 3001;
 
+// C O R S
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("submit-form-nat", (req, res) => {
+app.post("/submit-form-nat", (req, res) => {
   console.log(req.body); // log the data
   res.send("Data Received jo");
 });
