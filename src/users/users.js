a097a0212cf20e7d72501a6dfd006b3c1d4771a1
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-16 23:29:25 +0000

    add if statment. check if the array users, has a 'name' that matches the variable, if returns true, it executes

--- src/users/users.js
+++ src/users/users.js
@@ -1,6 +1,10 @@
 let users = [];
 
-const registerUser = (name, password) => {};
+const registerUser = (name, password) => {
+  if (users.some((user) => user.name === name)) {
+    return { success: false, message: "Username already taken" };
+  }
+};
 
 const loginUser = (name, password) => {};
 
