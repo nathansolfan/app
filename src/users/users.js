713de17ece74f7eeee01c9dfdf52a728f3bc7c3e
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-16 23:35:00 +0000

    update the loginUser variable. Create user, use find method to search the user and password

--- src/users/users.js
+++ src/users/users.js
@@ -4,8 +4,16 @@ const registerUser = (name, password) => {
   if (users.some((user) => user.name === name)) {
     return { success: false, message: "Username already taken" };
   }
+
+  users.push({ name, password });
+  return { success: true, message: "User registrou correctly" };
 };
 
-const loginUser = (name, password) => {};
+const loginUser = (name, password) => {
+    const user = users.find(user => user.name === name && user.password === password)
+    if(){
+        
+    }
+};
 
 module.exports = { registerUser, loginUser };
