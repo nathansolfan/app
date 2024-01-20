cf0d52befed0c9fd4aec233ed9b44faf181be5d1
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-20 22:09:25 +0000

    redoing it i basically import the form into register.js and add name, password as props

--- src/Register.js
+++ src/Register.js
@@ -2,7 +2,13 @@ import React from "react";
 import Form from "./form/form";
 
 export default function Register() {
-  const handleRegister = () => {};
+  const handleRegister = async (name, password) => {
+    try {
+      const response = await fetch("http://localhost:3001/register", {});
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
   return (
     <div>
       <div>Register</div>
