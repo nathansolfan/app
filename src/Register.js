8ffbd6544ca4f465fd1101518904e9642b44d6da
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-20 22:06:11 +0000

    Change the fetch function from the form.js into register to keep it cleaner

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Form from "./form/form";
 
 export default function Register() {
+  const handleRegister = () => {};
   return (
     <div>
       <div>Register</div>
