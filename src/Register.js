d86e707e76def904cd423402c2f85c738a84131a
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-20 22:10:59 +0000

    add the fetch request which include 2 parameters: 1 - https 2 - the method

--- src/Register.js
+++ src/Register.js
@@ -4,7 +4,13 @@ import Form from "./form/form";
 export default function Register() {
   const handleRegister = async (name, password) => {
     try {
-      const response = await fetch("http://localhost:3001/register", {});
+      const response = await fetch("http://localhost:3001/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, password }),
+      });
     } catch (error) {
       console.log("Error", error);
     }
