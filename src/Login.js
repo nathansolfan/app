d7b2edb1ec65a3ff3d972d154c610c1c2ca7c6d9
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-21 19:46:54 +0000

    update the log in file

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,23 @@
 import React from "react";
+import Form from "./form/form";
 
 export default function Login() {
-  return <form action=""></form>;
+  const handleLogin = async (name, password) => {
+    try {
+      const response = await fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, password }),
+      });
+    } catch (error) {
+      console.log("Error man", error);
+    }
+  };
+  return (
+    <div>
+      <Form onSubmit={handleLogIn} buttonLabel={"Login"} />
+    </div>
+  );
 }
