c33afea4a90df45241eeb3a9029de2c0920d16a2
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-28 23:36:39 +0000

    added h1, paragraph and an image with its classNames for styling

--- src/About.js
+++ src/About.js
@@ -4,6 +4,12 @@ const About = () => {
 
 return (
 <div className="about-container">
+  <h1 className="about-header"> About Me </h1>
+  <img className="profile-pic"></img>
+  <p className="introduction">
+    Hello, I`m Nathan. I`ve been studying coding for a long time.
+  </p>
+
 </div>
 )
 };
