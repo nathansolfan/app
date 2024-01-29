c6643ee9e29f46d817860deb29bb8f9e5db82b2c
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-01-29 20:02:56 +0000

    Updated Homepage to display the services i offer

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,30 @@
-const Home = () => {
+import React from "react";
+
+export default function Home() {
   return (
-    <div className="fruits">
-      <section>
-        <label htmlFor="fruit-list">Fruits:</label>
-        <ul id="fruit-list" className="fruit-list">
-          <li>Banana</li>
-          <li>Pear</li>
-          <li>Apple</li>
-          <li>Kiwi</li>
-        </ul>
+    <div className="home-container">
+      <header className="home-header">
+        <h1>Welcome to Lift Services</h1>
+        <p>Reliable and comfortable rides to your destination</p>
+      </header>
+
+      <section className="services">
+        <h2>Our services:</h2>
+        <div>
+          <h3>Work Commute</h3>
+          <p>
+            Start your workday stress-free with a timely and comfortable ride to
+            work
+          </p>
+        </div>
+        <div>
+          <h3>Aiport Transfers</h3>
+          <p>
+            Ensure a smooth ride to and from the airport for those times where
+            bus and trains are not available.
+          </p>
+        </div>
       </section>
     </div>
   );
-};
-
-export default Home;
+}
