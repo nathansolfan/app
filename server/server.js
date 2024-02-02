74069278a458a75e82531b1da66b2aa5414f9628
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-02 16:42:22 +0000

    create fs and path const from fs/path. and BOOKINGS_FILE const to the json file

--- server/server.js
+++ server/server.js
@@ -13,6 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// FS and Path
+const fs = require("fs");
+const path = require("path");
+
+const BOOKINGS_FILE = path.join(__dirname, "bookings.json");
+
 app.post("/submit-form-nat", (req, res) => {
   console.log(req.body); // log the data
   res.send("Data Received jo");
