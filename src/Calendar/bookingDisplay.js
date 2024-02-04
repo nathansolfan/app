e37a500d930d572bf21960956878bdb12364da40
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-04 14:02:41 +0000

    create new component to display the booked dates. start with usestate and useeffect( () => {} )

---
+++ src/Calendar/bookingDisplay.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+export default function bookingDisplay() {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {});
+
+  return <div></div>;
+}
