b1e8eace329e2d62f21499e350647b932bbbfc29
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-04 14:03:44 +0000

    fetch the http://localhost:3001/ the page created

--- src/Calendar/bookingDisplay.js
+++ src/Calendar/bookingDisplay.js
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from "react";
 export default function bookingDisplay() {
   const [bookings, setBookings] = useState([]);
 
-  useEffect(() => {});
+  useEffect(() => {
+    fetch("http://localhost:3001/bookings");
+  });
 
   return <div></div>;
 }
