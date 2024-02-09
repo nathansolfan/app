78f123cf7c8d128a42618c4c5dd8778738851824
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-09 15:36:00 +0000

    add .then() response as its received from the fetch and parse it to json. .then() also the data, and set it to the new state. Then catch error if any

--- src/Calendar/FeedbackList.js
+++ src/Calendar/FeedbackList.js
@@ -1,10 +1,14 @@
+import { response } from "express";
 import React, { useEffect, useState } from "react";
 
 export default function FeedbackList() {
   const [feedbackList, setFeebackList] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3001/api/feedback").then();
+    fetch("http://localhost:3001/api/feedback")
+      .then((response) => response.json())
+      .then((data) => setFeebackList(data))
+      .catch((error) => console.error("Error fetching DA feedback", error));
   });
 
   return <div>FeedbackList</div>;
