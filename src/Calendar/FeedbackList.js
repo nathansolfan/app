f47c2dd59a15b2d5f3afebc60b1ff89ad0d7dc5d
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-09 15:31:15 +0000

    crea useEffect( () => {} ) start by fetch( the /api/feedback )

--- src/Calendar/FeedbackList.js
+++ src/Calendar/FeedbackList.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function FeedbackList() {
   const [feedbackList, setFeebackList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/feedback").then();
+  });
+
   return <div>FeedbackList</div>;
 }
