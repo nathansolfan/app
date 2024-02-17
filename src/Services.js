56c115bd4e89e247b6ce14fb25a0508b35a3811b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-17 23:42:39 +0000

    use the formula section*8>(h1>{Name})+p>lorem

--- src/Services.js
+++ src/Services.js
@@ -1,5 +1,80 @@
 const Services = () => {
-  return <div>Services i offer</div>;
+  return (
+    <div>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum
+          nobis adipisci natus, maxime quia dolores minus perspiciatis hic eos
+          non quisquam, a labore cupiditate repellat, rerum fugit ab?
+          Perspiciatis?
+        </p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>
+          Sequi quisquam rerum, in, quasi veritatis qui quidem reiciendis
+          aliquam sed perspiciatis facere amet sapiente temporibus architecto
+          voluptate aspernatur consequatur iusto praesentium officia? Veniam
+          omnis debitis fuga soluta porro excepturi.
+        </p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>
+          At possimus minus, debitis veniam illum saepe velit amet dolore
+          aspernatur corrupti, corporis eius minima maiores veritatis nisi?
+          Tenetur ratione eveniet quos magnam ipsum? Impedit facere praesentium
+          quas soluta velit.
+        </p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>
+          Adipisci est amet, iste aliquid, deserunt consequuntur quo unde quae
+          quaerat odio ratione? Doloremque dolorem velit, quibusdam voluptates
+          commodi ad, nemo, laborum quis itaque consequatur praesentium atque
+          culpa. Labore, nobis.
+        </p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>
+          Perspiciatis molestias veniam, maxime quae debitis unde labore
+          obcaecati nisi iure consectetur esse corporis rerum cupiditate hic
+          sapiente. Mollitia temporibus eaque excepturi quis repellat. Est
+          aliquid quaerat expedita blanditiis vero.
+        </p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>
+          Pariatur vitae unde cupiditate maiores ullam quos neque harum rem
+          inventore eos, corporis, ab consectetur eaque ea, deleniti nulla
+          soluta expedita. Quaerat, veniam. Nesciunt repellendus fuga ullam
+          similique aliquam vel!
+        </p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>
+          At tenetur enim natus reprehenderit hic molestiae corrupti sed iste
+          recusandae, dolorem officia veniam laudantium, nesciunt sit excepturi
+          neque sint, quos temporibus! Natus, quaerat amet officia vero at unde
+          omnis.
+        </p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>
+          Alias magnam dignissimos qui nihil assumenda neque eligendi, modi
+          rerum fuga molestias soluta culpa dolore molestiae sint dolor illum
+          debitis aliquam, numquam, perferendis atque incidunt voluptatem nobis
+          quasi! Veniam, incidunt?
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default Services;
