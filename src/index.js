254ecc9b4cb4b3dfd1a5ae9d87e0dda73f3389c3
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-19 23:18:34 +0000

    insert cdn script for kute.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+<script src="https://cdn.jsdelivr.net/npm/kute.js@2.2.4/dist/kute.min.js"></script>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
