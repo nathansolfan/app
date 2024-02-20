f6506692b732dcc32b7af3f5ee3d5431f3585da5
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-20 23:36:10 +0000

    Added Lorem

--- src/Services/Line.js
+++ src/Services/Line.js
@@ -6,6 +6,11 @@ export default function Line() {
   return (
     <div className="service-section">
       <div className="curve"></div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quod modi
+        autem placeat consectetur commodi nam animi harum aut nemo corrupti
+        earum veritatis amet consequuntur molestiae inventore ad, fugit laborum.
+      </p>
     </div>
   );
 }
